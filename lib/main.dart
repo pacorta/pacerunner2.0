c84976956db36c093edbd63caf62839bf0c960c2
Paco Orta Bazán <pacoortabazan@Pacos-Laptop.local>
2024-07-08 20:03:15 -0500

    I added the necessary changes to integrate the location package. Modified files include: pubspec.yaml, androidManifest.xml for the permissions in android, and info.plist for permissions in ios.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'The Paced Runner Home Screen'),
+      home: const MyHomePage(title: 'The Pacerunner Home Screen'),
     );
   }
 }
